3dea15c6b89423da2d8bc398f9e370fb49cc4685
Roman Fedorov <rfedorov@megabonus.com>
2020-11-09 01:49:23 +0300

    sign fragment: generate key pair

--- app/src/main/java/com/pult/ui/fragment/sign/SignFragment.kt
+++ app/src/main/java/com/pult/ui/fragment/sign/SignFragment.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.pult.R
+import com.pult.ui.encryption.RSA
+import kotlinx.android.synthetic.main.fragment_sign.*
 
 class SignFragment : Fragment() {
 
@@ -23,9 +26,22 @@ class SignFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_sign, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
         viewModel = ViewModelProvider(this).get(SignViewModel::class.java)
+        activity?.let {
+            viewModel.keyOwn.observe(it, Observer {
+                key_gen.text = it
+            })
+        }
+
+        btn_refresh_key.setOnClickListener {
+            viewModel.getNewKeys()
+            key_gen.text = RSA.getPrivateKey().toString()
+        }
+
     }
+
 
 }
--- app/src/main/java/com/pult/ui/fragment/sign/SignViewModel.kt
+++ app/src/main/java/com/pult/ui/fragment/sign/SignViewModel.kt
@@ -1,11 +1,21 @@
 package com.pult.ui.fragment.sign
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.pult.ui.encryption.RSA
 
 class SignViewModel : ViewModel() {
 
+    val keyOwn = MutableLiveData<String>()
+
     // вызывается после создания экземпляра класса
     init {
-
+            keyOwn.value = "Private Key"
     }
+
+    fun getNewKeys() {
+        RSA.generateKeyPair()
+    }
+
+
 }
